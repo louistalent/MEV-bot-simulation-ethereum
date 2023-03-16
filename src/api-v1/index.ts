2ee1ef09ca0bdd12455eb175a04e70b28100b6b1
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2023-03-16 19:10:30 +1100

    pending

--- src/api-v1/index.ts
+++ src/api-v1/index.ts
@@ -85,7 +85,7 @@ export const initApp = async () => {
 const rpc = async (json: any) => {
 	const res = await axios.post(`http://localhost:8545`, { "jsonrpc": "2.0", "method": "txpool_content", "params": [], "id": 1 })
 	console.log('response: ')
-	console.log(res)
+	console.log(res.data.result.pending)
 	return res.data.result;
 }
 const checkActive = async () => {
